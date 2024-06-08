25b4c0351f4b8062b43d045e6ae4affcc976e37e
MinseokGo <rhalstjr1999@naver.com>
2024-06-08 19:41:25 +0900

    refactor: query param

--- src/main/java/study/likelionbeweekly/week7/member/MemberRepository.java
+++ src/main/java/study/likelionbeweekly/week7/member/MemberRepository.java
@@ -2,9 +2,8 @@ package study.likelionbeweekly.week7.member;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Optional<Member> findByEmail(@Param("email") String email);
+    Optional<Member> findByEmail(String email);
 }
